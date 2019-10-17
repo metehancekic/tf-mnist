ff75fe9aaec15b229d96b12c458925793824304e
Metehan Cekic <metehancekic@umail.ucsb.edu>
2019-10-17 00:28:08 -0700

    Updated low_API

--- MNIST/low_API.py
+++ MNIST/low_API.py
@@ -12,24 +12,6 @@ import numpy as np
 from tqdm import tqdm     # Adds Progress bar to loops
 
 
-tf.reset_default_graph()                        # To reset any existing graphs
-tf.logging.set_verbosity(tf.logging.ERROR)      # If you don't want to see too much warnings, put this !!
-
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
-# print(mnist.test.images.shape)
-# print(mnist.train.images.shape)
-
-images, labels = mnist.train.next_batch(100)
-
-# print(images.shape)
-# print(labels.shape)
-# print(mnist.validation.images.shape)          # Checks the shape of datasets
-
-# plt.imshow(images[0].reshape([28,28]))        # Plot a random image from dataset
-# plt.show()
-
-
 '''
 Why Low API?
 
@@ -50,9 +32,8 @@ There are 3 important things in tensorflow:
 3) hyper-parameters : parameters that we tune manually ( learning rate, number of iterations, regularization term, keeping prob, etc. )
 
 '''
-
         
-def model(x, prob):
+def model(x, prob):                                         # Neural Model
 
     with tf.variable_scope("CNN", reuse = tf.AUTO_REUSE):       
 
@@ -97,113 +78,134 @@ def model(x, prob):
     return o5 #,w1  # To be able to inspect w1, you need to give as output of this function, and run a session to get values.
 
 
-################# BUILDING GRAPH ###################
-# Placeholders are input nodes for our graph.
+def main():                                         # Main function to run
+    
+    tf.reset_default_graph()                        # To reset any existing graphs
+    tf.logging.set_verbosity(tf.logging.ERROR)      # If you don't want to see too much warnings, put this !!
 
-x = tf.placeholder(tf.float32, shape = [None, 28, 28, 1], name ="inputs")
-y_actual = tf.placeholder(tf.float32, shape = [None, 10], name ="labels")
-prob = tf.placeholder_with_default(1.0, shape=(), name = "prob")  # Placeholder with default value, 
-# if you don't specify in feed_dict it will assume prob = 1.0
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
+    # print(mnist.test.images.shape)
+    # print(mnist.train.images.shape)
 
-# We need logits to get predictions.
-logits = model(x, prob)
-# logits, w1 = model(x, prob)
+    images, labels = mnist.train.next_batch(100)
 
-Y = tf.nn.softmax(logits)      
+    # print(images.shape)
+    # print(labels.shape)
+    # print(mnist.validation.images.shape)          # Checks the shape of datasets
 
-# Cross Entropy takes logits and actual labels as input.
-cross_entropy = tf.losses.softmax_cross_entropy(onehot_labels = y_actual, logits = logits)
-total_loss = tf.losses.get_total_loss(add_regularization_losses = True)
+    # plt.imshow(images[0].reshape([28,28]))        # Plot a random image from dataset
+    # plt.show()
 
+    ################# BUILDING GRAPH ###################
+    # Placeholders are input nodes for our graph.
 
-## OPTIMIZER: Updates weights according to backprop algorithm
-# There are options: ADAM, SGD, MOMENTUM SGD ...
-
-# optimizer = tf.train.AdamOptimizer( learning_rate = 0.001, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-3).minimize(total_loss)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(total_loss)
-# optimizer = tf.train.MomentumOptimizer(learning_rate=0.1, momentum = 0.1).minimize(total_loss)
-
-## We need to assess our neural net, whether it learns or not. We can evaluate our model by defining accuracy node which uses prediction node.. 
-
-prediction = tf.equal(tf.argmax(logits,1), tf.argmax(y_actual, 1)) # Don't forget we have one hot labels !!
-accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))  # Just get the accuracy
-
-# GRAPH IS DONE.. What else?
-# Let's execute the graph by feeding data.
-
-# We need to start a session
-sess = tf.InteractiveSession()
-
-# Initialize all the variables in our graph.
-sess.run(tf.global_variables_initializer())
-
-nb_classes = 10 # Number of classes
-
-# Hyper parameters for our model
-num_iter = 200
-load_from_checkpoint = False     # If you don't want to train and just use a checkpoint, set this to true
-batch_size = 100
-
-if not load_from_checkpoint:
-    for i in tqdm(range(num_iter)):     # tqdm shows a progress bar on your terminal for the for loop..
-
-        images, labels = mnist.train.next_batch(batch_size) # Get batches to feed in and execute "optimizer" node
-
-        train_data = {                                      # We need to feed data as a dictionary ( placeholders will be keys of   
-            x: images.reshape([-1,28,28,1]),                # dictionary ) to our graph to execute a specific node.
-            y_actual: labels,
-            prob: 0.5}                                      # Keep probability of dropout
-
-        sess.run(optimizer, feed_dict=train_data)           # Optimizer is executed here
+    x = tf.placeholder(tf.float32, shape = [None, 28, 28, 1], name ="inputs")
+    y_actual = tf.placeholder(tf.float32, shape = [None, 10], name ="labels")
+    prob = tf.placeholder_with_default(1.0, shape=(), name = "prob")  # Placeholder with default value, 
+    # if you don't specify in feed_dict it will assume prob = 1.0
 
 
-        if i % np.int(num_iter/10) == 0:                    # Every num_iter/10 th iteration we check validation accuracy
+    # We need logits to get predictions.
+    logits = model(x, prob)
+    # logits, w1 = model(x, prob)
 
-            validation_data = {
-    		    x: mnist.validation.images.reshape([-1,28,28,1]), 
-    		    y_actual: mnist.validation.labels}
+    Y = tf.nn.softmax(logits)      
 
-            acc_val = sess.run(accuracy, feed_dict=validation_data)
-
-            print('\nValidation accuracy: {0:.2f}%'.format(acc_val*100))
-        
-
-    test_data = {
-        x: mnist.test.images.reshape([-1,28,28,1]), 
-        y_actual: mnist.test.labels}
-
-    acc = sess.run(accuracy, feed_dict=test_data)       # After training we evaluate model by computing test accuracy
-
-    print('Final Test accuracy: {0:.2f}% '.format(acc*100))
-
-    save_dir = "./checkpoints/mnist" + ".ckpt"          # Saving checkpoints in following folder
-    saver = tf.train.Saver()
-    save_path = saver.save(sess, save_dir)              # Save checkpoints
-
-else:
- 
-    res_dir = "./checkpoints/mnist" + ".ckpt"
-    saver = tf.train.Saver(var_list = tf.trainable_variables("CNN"))   # Restoring checkpoints in following folder
-
-    # chkp.print_tensors_in_checkpoint_file(res_dir, tensor_name='', all_tensors=True)                      # Inspect checkpoint (Prints out all the tensors saved in checkpoint)
-    # chkp.print_tensors_in_checkpoint_file(res_dir, tensor_name='CNN/layer1/weight', all_tensors=False)    # Inspect checkpoint (Prints out tensors with specified name saved in checkpoint)
-
-    saver.restore(sess, res_dir)                        # Restore checkpoints
-
-    test_data = {
-        x: mnist.test.images.reshape([-1,28,28,1]), 
-        y_actual: mnist.test.labels}
-
-    acc = sess.run(accuracy, feed_dict=test_data)
-
-    print('Checkpoint Test accuracy: {0:.2f}% '.format(acc*100))
+    # Cross Entropy takes logits and actual labels as input.
+    cross_entropy = tf.losses.softmax_cross_entropy(onehot_labels = y_actual, logits = logits)
+    total_loss = tf.losses.get_total_loss(add_regularization_losses = True)
 
 
-# w1 = sess.run(w1, feed_dict = test_data)          # If you want to inspect any parameter defined in graph you can just run the 
-                                                    # graph for that variable
+    ## OPTIMIZER: Updates weights according to backprop algorithm
+    # There are options: ADAM, SGD, MOMENTUM SGD ...
+
+    # optimizer = tf.train.AdamOptimizer( learning_rate = 0.001, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-3).minimize(total_loss)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(total_loss)
+    # optimizer = tf.train.MomentumOptimizer(learning_rate=0.1, momentum = 0.1).minimize(total_loss)
+
+    ## We need to assess our neural net, whether it learns or not. We can evaluate our model by defining accuracy node which uses prediction node.. 
+
+    prediction = tf.equal(tf.argmax(logits,1), tf.argmax(y_actual, 1)) # Don't forget we have one hot labels !!
+    accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))  # Just get the accuracy
+
+    # GRAPH IS DONE.. What else?
+    # Let's execute the graph by feeding data.
+
+    # We need to start a session
+    sess = tf.InteractiveSession()
+
+    # Initialize all the variables in our graph.
+    sess.run(tf.global_variables_initializer())
+
+    nb_classes = 10 # Number of classes
+
+    # Hyper parameters for our model
+    num_iter = 200
+    load_from_checkpoint = False     # If you don't want to train and just use a checkpoint, set this to true
+    batch_size = 100
+
+    if not load_from_checkpoint:
+        for i in tqdm(range(num_iter)):     # tqdm shows a progress bar on your terminal for the for loop..
+
+            images, labels = mnist.train.next_batch(batch_size) # Get batches to feed in and execute "optimizer" node
+
+            train_data = {                                      # We need to feed data as a dictionary ( placeholders will be keys of   
+                x: images.reshape([-1,28,28,1]),                # dictionary ) to our graph to execute a specific node.
+                y_actual: labels,
+                prob: 0.5}                                      # Keep probability of dropout
+
+            sess.run(optimizer, feed_dict=train_data)           # Optimizer is executed here
 
 
+            if i % np.int(num_iter/10) == 0:                    # Every num_iter/10 th iteration we check validation accuracy
+
+                validation_data = {
+        		    x: mnist.validation.images.reshape([-1,28,28,1]), 
+        		    y_actual: mnist.validation.labels}
+
+                acc_val = sess.run(accuracy, feed_dict=validation_data)
+
+                print('\nValidation accuracy: {0:.2f}%'.format(acc_val*100))
+            
+
+        test_data = {
+            x: mnist.test.images.reshape([-1,28,28,1]), 
+            y_actual: mnist.test.labels}
+
+        acc = sess.run(accuracy, feed_dict=test_data)       # After training we evaluate model by computing test accuracy
+
+        print('Final Test accuracy: {0:.2f}% '.format(acc*100))
+
+        save_dir = "./checkpoints/mnist" + ".ckpt"          # Saving checkpoints in following folder
+        saver = tf.train.Saver()
+        save_path = saver.save(sess, save_dir)              # Save checkpoints
+
+    else:
+     
+        res_dir = "./checkpoints/mnist" + ".ckpt"
+        saver = tf.train.Saver(var_list = tf.trainable_variables("CNN"))   # Restoring checkpoints in following folder
+
+        # chkp.print_tensors_in_checkpoint_file(res_dir, tensor_name='', all_tensors=True)                      # Inspect checkpoint (Prints out all the tensors saved in checkpoint)
+        # chkp.print_tensors_in_checkpoint_file(res_dir, tensor_name='CNN/layer1/weight', all_tensors=False)    # Inspect checkpoint (Prints out tensors with specified name saved in checkpoint)
+
+        saver.restore(sess, res_dir)                        # Restore checkpoints
+
+        test_data = {
+            x: mnist.test.images.reshape([-1,28,28,1]), 
+            y_actual: mnist.test.labels}
+
+        acc = sess.run(accuracy, feed_dict=test_data)
+
+        print('Checkpoint Test accuracy: {0:.2f}% '.format(acc*100))
+
+
+    # w1 = sess.run(w1, feed_dict = test_data)          # If you want to inspect any parameter defined in graph you can just run the 
+                                                        # graph for that variable
+
+if __name__ == '__main__':                          # When you run low_API.py, following code will be executed
+    
+    main()
 
 
 
--- MNIST/readme.md
+++ MNIST/readme.md
@@ -1,3 +1,10 @@
 ## 4 Layer CNN
 A simple tutorial of MNIST classification with 4 layer convolutional neural network (CNN). Neural network is implemented with low-api tensorflow modules.
 
+**Libraries Needed**
+
+- Tensorflow 		1.10.0
+- Matplotlib 		3.0.2
+- Numpy			1.16.3
+- Tqdm (Optional) 	4.28.1
+
